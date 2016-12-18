3388775cfa4db1a78060227db01d76db1847dc1a
fizikst <fizikst@mail.ru>
2016-12-18 23:57:05 +0400

    fix

--- mdn/capture.js
+++ mdn/capture.js
@@ -48,6 +48,7 @@ $.ajax({
     url: "http://35.162.213.153/drivers/photoSave",
     data: { img_data:data, drivers_id:10 },
     cache: false,
+    async: false,
     contentType: "application/x-www-form-urlencoded",
     success: function (result) {
         alert(result);
